2a0c923af782426ad4c2e6403995cec2e7972b25
German Vargin <german.vargin@chirpwireless.io>
2025-01-13 13:55:38 +0100

    updated fixtures

--- tests/test_titles.py
+++ tests/test_titles.py
@@ -1,11 +1,12 @@
 from playwright.sync_api import Page, expect
 
 
-def test_main_page_title(page):
-
+def test_main_page_title(page: Page):
     assert page.title() == "Литрес – сервис электронных и аудиокниг, скачать в fb2 и mp3, читать и слушать онлайн на Litres"
 
-def test_audiobook_page_title(page):
-
+def test_audiobook_page_title(page: Page):
     page.locator("xpath=//*[@id=\"lowerMenuWrap\"]/nav/div/a[6]").click()
     expect(page).to_have_title("Аудиокниги – слушать онлайн или скачать в mp3 на Литрес")
+
+
+
