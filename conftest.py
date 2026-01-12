8c05ab6b788f487a943c1920ce2fe1412e32e65d
German Vargin <german.vargin@chirpwireless.io>
2026-01-12 14:08:11 +0100

    added test data

--- conftest.py
+++ conftest.py
@@ -1,12 +1,42 @@
+import json
+from typing import Optional
+
 from playwright.sync_api import Page
 import pytest
-import json
-from pathlib import Path
-
 from pages.home_page import HomePage
 from pages.search_results_page import SearchResultsPage
+from pathlib import Path
+from dataclasses import dataclass
+from typing import Optional, Iterable
 
+# TEST DATA
 DATA_DIR = Path(__file__).parent / "test_data"
+
+@dataclass
+class SearchTestDada:
+    id: str
+    query: str
+    expected_count: str
+
+def load_search_test_data(ids: Optional[Iterable[str]] = None):
+
+    with open(DATA_DIR / "search_data.json", encoding="utf-8") as f:
+        raw_data = json.load(f)["search_input"]
+
+    if ids is not None:
+
+        raw_data = [item for item in raw_data if item["id"] in ids]
+
+    return [
+        SearchTestDada(
+            id=item["id"],
+            query=item["query"],
+            expected_count=item["expected_results_count"]
+        )
+        for item in raw_data
+    ]
+
+
 
 
 @pytest.fixture(autouse=True)
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,23 +1,22 @@
-from playwright.sync_api import Page, expect
 import pytest
-from conftest import results
-from urllib.parse import quote, quote_plus
+from playwright.sync_api import expect
+from conftest import results, load_search_test_data
 
 
-def test_main_actions(page, home, results, query):
-    query = "python"
+@pytest.mark.parametrize("query", ["Python", "Игра Престолов", "Стивен Кинг"])
+def test_filters(home, results, query):
     home.search(query, True)
-
-    expect(page).to_have_url(f"https://www.litres.ru/search/?q={quote_plus(query)}")
-
     results.apply_russian_filter()
     expect(results.russian_chip).to_be_visible()
 
+@pytest.mark.parametrize("td", load_search_test_data(ids=["python_books", "game_of_thrones"]))
+def test_filters2(home, results, td):
+    home.search(td.query, True)
+    results.apply_russian_filter()
+    expect(results.russian_chip).to_be_visible()
 
-def test_waiting(page, home, results):
-    query = "Самоучитель Python"
-    home.search(query)
-
-    expect(page).to_have_url(f"https://www.litres.ru/search/?q={quote_plus(query)}")
-    expect(results.results_title).to_contain_text(query)
-    expect(results.books).to_have_count(3, timeout=10000)
+@pytest.mark.parametrize("td", load_search_test_data(ids=["richard_bransen", "stephen_king"]))
+def test_results_count(home, results, td):
+    home.search(td.query)
+    expect(results.results_title).to_contain_text(td.query)
+    expect(results.books).to_have_count(td.expected_count, timeout=10000)
